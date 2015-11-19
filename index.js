020d560b70ed4d82abc25a0c7bc0530fda5cb2d6
Anirudh Ruia Gali <gali.anirudh@gmail.com>
2015-11-19 16:38:34 -0700

    chapter 3

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ var _ = require('underscore');
 _.each([1, 2, 3], function (v) {
     console.log(v);
 });
+
+
+var mongodb = require('mongodb');
+
+var uri = 'mongodb://localhost:27017/test';
+mongodb.MongoClient.connect(uri, function (error, db) {
+    if (error) {
+        console.log(error);
+        process.exit(1);
+    }
+
+    db.collection('sample').insert({
+        y: 1
+    }, function (error, result) {
+        if (error) {
+            console.log(error);
+            process.exit(1);
+        }
+
+        db.collection('sample').find().toArray(function (error, docs) {
+            if (error) {
+                console.log(error);
+                process.exit(1);
+            }
+
+            console.log('Found docs:');
+            docs.forEach(function (doc) {
+                console.log(JSON.stringify(doc));
+            });
+            process.exit(0);
+        });
+    });
+});
+
